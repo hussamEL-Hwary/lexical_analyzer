44279ebe5e5cba336bcf40078d2f3c058ab3290b
hussamEL-Hwary <hossam.elhwary@stud.fci-cu.edu.eg>
2019-03-18 22:56:06 +0200

    add Expression class

---
+++ src/main/Expression.java
@@ -0,0 +1,12 @@
+package main;
+
+public class Expression {
+	public String type;
+	public String expRule;
+	
+	public Expression(String type, String exp) {
+		this.type = type;
+		this.expRule = exp;
+	}
+	
+}
