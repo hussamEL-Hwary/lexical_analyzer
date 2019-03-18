cfc3b424282de3c2b4a37eaac6a7e90b5d390a7e
hussamEL-Hwary <hossam.elhwary@stud.fci-cu.edu.eg>
2019-03-18 18:21:14 +0200

    read file content

---
+++ src/main/Helper.java
@@ -0,0 +1,28 @@
+package main;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Helper {
+
+	/**
+	 * Reading file content and returning it as a string
+	 * 
+	 * @param filePath 
+	 * @return string of the file content
+	 * */
+	public String read_file_content(String filePath) throws IOException {
+		BufferedReader reader = new BufferedReader(new FileReader(filePath));
+		StringBuilder content = new StringBuilder();
+		String line = null;
+		String ls = System.getProperty("line.separator");
+		while((line=reader.readLine()) != null){
+			content.append(line);
+			content.append(ls);
+		}
+		reader.close();
+		return content.toString();
+	}
+}
---
+++ src/main/Lexer.java
@@ -0,0 +1,14 @@
+package main;
+import java.io.IOException;
+
+import main.Helper;
+public class Lexer {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		Helper h = new Helper();
+		System.out.println(h.read_file_content("test.txt"));
+		
+	}
+
+}
