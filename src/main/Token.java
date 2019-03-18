44667070dc397614b82cd185a9bc2b638aed4fc6
hussamEL-Hwary <hossam.elhwary@stud.fci-cu.edu.eg>
2019-03-18 22:55:21 +0200

    token class

---
+++ src/main/Token.java
@@ -0,0 +1,12 @@
+package main;
+
+public class Token {
+	public String type;
+	public String lexeme;
+	
+	public Token(String type, String lexeme)
+	{
+		this.type = type;
+		this.lexeme = lexeme;
+	}
+}
