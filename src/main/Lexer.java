f8890786f833688b10fbd1fa44fcd7aee6fd2fe4
hussamEL-Hwary <hossam.elhwary@stud.fci-cu.edu.eg>
2019-03-20 15:02:07 +0200

    init regex rules

--- src/main/Lexer.java
+++ src/main/Lexer.java
@@ -1,14 +1,21 @@
 package main;
 import java.io.IOException;
-
+import java.util.regex.Pattern;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
 import main.Helper;
+
 public class Lexer {
+	static ArrayList<Expression> regexRules = new ArrayList<Expression>();
+	
+	public static void initRules(){
+		regexRules.add(new Expression("LineComment","(//(.*?)[\r$]?\n).*"));
+		regexRules.add(new Expression("Email", "(\\w+)@(\\w+\\.)(\\w+)(\\.\\w+)*"));
+	}
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		Helper h = new Helper();
-		System.out.println(h.read_file_content("test.txt"));
-		
+		initRules();
 	}
 
 }
